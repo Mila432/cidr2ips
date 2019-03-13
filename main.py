f9b1a58131378006b500fa3eaadca11728dc858b
root <root@root>
2019-03-13 22:34:22 +0900

    file

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import struct
 import sys
 
 def save(d,f):
-	with io.open(f, 'w', encoding='utf8') as the_file:
+	with io.open(f, 'a', encoding='utf8') as the_file:
 		the_file.write('%s'%(unicode(d)))
 
 def ip2hex(ip='127.0.0.1'):
@@ -17,11 +17,14 @@ def hex2ip(hex_ip):
 def getmax(n2):
 	return 2**(32-int(n2))
 
-max=getmax(sys.argv[1].split('/')[-1])
-
 ips=set([])
+with open(sys.argv[1]) as f:
+	content = f.readlines()
+	content = [x.strip() for x in content]
+for cidr in content:
+	max=getmax(cidr.split('/')[-1])
 
-ip=ip2hex(sys.argv[1].split('/')[0])
-while(len(ips)<max):
-	ips.add(ip+len(ips))
-save('\n'.join([hex2ip(x) for x in ips]),'ips.txt')
+	ip=ip2hex(cidr.split('/')[0])
+	while(len(ips)<max):
+		ips.add(ip+len(ips))
+	save('\n'.join([hex2ip(x) for x in ips]),'ips.txt')
